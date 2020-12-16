424b59704ea5ad6b6d62a19094163635eea07ae5
zdxzdx1219 <769371987@qq.com>
2020-12-16 14:58:27 +0800

    Money 页面布局

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@
 <script lang="ts">
 </script>
 <style lang="scss">
-
+    *{
+        margin: 0;
+        padding:0 ;
+        box-sizing: border-box;
+    }
     #app {
         font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+       <p> Money.vue</p>
+        </div>
         <Nav/>
      </div>
 
@@ -13,5 +58,15 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        border: 1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        border: 1px solid blue;
+        overflow: auto;
+        flex-grow: 1;
+    }
 </style>
