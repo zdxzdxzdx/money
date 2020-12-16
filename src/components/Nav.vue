fe16ca307ab5f1438bf8f7a36065686fad45d3fa
zdxzdx1219 <769371987@qq.com>
2020-12-16 13:50:24 +0800

    全局组件<Nav/>

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>$END$</div>
+    <div>
+        <router-link to="/money">记账</router-link>|
+        <router-link to="/labels">标签</router-link>|
+        <router-link to="/statistics">统计</router-link>
+    </div>
+
 </template>
 
 <script lang="ts">
