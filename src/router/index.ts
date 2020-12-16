5ede74d4e10a8ece926a4e4da0511463e5f11f56
zdxzdx1219 <769371987@qq.com>
2020-12-16 14:14:47 +0800

    添加 404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
-import Home from '@/views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import Notfound from '@/views/Notfound.vue';
 
 Vue.use(VueRouter);
 
@@ -25,6 +25,10 @@ const routes: Array<RouteConfig> = [
     {
         path: '/statistics',
         component: Statistics
+    },
+    {
+        path:'*',
+        component:Notfound
     }
 
 ];
---
+++ src/views/Notfound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>当期页面不存在，请检查网址路径是否正确</div>
+        <div>
+            <router-link to="/">返回首页</router-link>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Notfound'
+    };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
