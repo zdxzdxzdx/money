1a88d6eab2f63a277643c9f4305c54d88260d0b8
zdxzdx1219 <769371987@qq.com>
2020-12-18 13:35:01 +0800

    添加全局样式和全局变量

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="app">
         <router-view/>
     </div>
 
@@ -11,39 +11,17 @@
 
 </script>
 <style lang="scss">
-    *{
-        margin: 0;
-        padding:0 ;
-        box-sizing: border-box;
-    }
-    body{
-        line-height: 1.5;
+    @import "~@/assets/style/helper.scss";
+    @import "~@/assets/style/reset.scss";
 
-    }
-    a{
-        text-decoration: none;
-        color: inherit;
-    }
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
+    body{
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
+        color: #333333;
+        line-height: 1.5;
+        font-family: $font-hei;
     }
 
-    #nav {
-        padding: 30px;
-
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-
-            &.router-link-exact-active {
-                color: #42b983;
-            }
-        }
-    }
 </style>
 
 
