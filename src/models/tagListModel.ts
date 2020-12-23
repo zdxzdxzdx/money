41fa41dfdcfef5b6f7d5ab8220450ec6f5552b34
zdxzdx1219 <769371987@qq.com>
2020-12-23 13:19:12 +0800

    将 tag的 string 改为{id,name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,9 +1,12 @@
 const localStorageKayName = 'tagList';
-
+type Tag={
+    id: string;
+    name: string;
+}
 type TagListModel = {
 
-    data: string[]
-    fetch: () =>  string[]
+    data: Tag[]
+    fetch: () =>  Tag[]
     create: (name: string) => 'success'| 'duplicated' // 联合类型 success表示成功 duplicated 表示 name 重复
     save: () => void
 
@@ -15,9 +18,9 @@ const tagListModel: TagListModel = {
         return this.data;
     },
     create(name) {
-
-        if (this.data.indexOf(name) >=0 ) {return 'duplicated'}
-        this.data.push(name)
+        const names=this.data.map(item=> item.name)
+        if (names.indexOf(name) >=0 ) {return 'duplicated'}
+        this.data.push({id:name,name:name})
         this.save()
         return 'success'
     },
