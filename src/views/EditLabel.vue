ee6d8829f7530cee0451b0afc9b0da17d6c28369
zdxzdx1219 <769371987@qq.com>
2020-12-23 13:45:57 +0800

    新增 EditLabel组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -5,10 +5,23 @@
 <script lang="ts">
     import Vue from 'vue';
     import {Component} from 'vue-property-decorator';
+    import tagListModel from '@/models/tagListModel';
 
     @Component
     export default class EditLable extends Vue {
 
+        created(){
+            console.log(this.$route.params);
+            const id=this.$route.params.id
+            tagListModel.fetch
+            const tags=tagListModel.data
+            const tag=tags.filter(t=>t.id === id)[0]
+            if(tag){
+                console.log(tag);
+            }else {
+                this.$router.replace('/404')
+            }
+        }
     }
 </script>
 
