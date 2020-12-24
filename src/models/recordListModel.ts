695ca54fddb9f9b51897455969936b1f06f38364
zdxzdx1219 <769371987@qq.com>
2020-12-24 09:45:09 +0800

    将 create 封装到 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,20 +1,24 @@
+import clone from '@/lib/clone';
+
 const localStorageKeyName = 'recordList';
 const recordListModel = {
     //保存 data
     data: [] as RecordItem[],
 
-    clone(data: RecordItem[] | RecordItem) {
-        return JSON.parse(JSON.stringify(data));
+    create(record: RecordItem){
+        const record2: RecordItem = clone(record);
+        record2.createdAt = new Date();
+        this.data.push(record2);
     },
     //读数据 写数据
     fetch() {
         //返回值
-        this.data = JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordItem[];
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
         return this.data;
 
     },
     save() {
-        window.localStorage.setItem('localStorageKeyName',
+        window.localStorage.setItem(localStorageKeyName,
             JSON.stringify(this.data));
     }
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -56,14 +56,12 @@
         }
 
         saveRecord() {
-            const record2: RecordItem = recordListModel.clone(this.record);
-            record2.createdAt = new Date();
-            this.recordList.push(record2);
+            recordListModel.create(this.record)
         }
 
         @Watch('recordList')
         onRecordChange() {
-            recordListModel.save(this.recordList);
+            recordListModel.save();
         }
 
     }
