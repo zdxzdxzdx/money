79c135da5ef06b0b7bfbdeac900b4cbd4aa51f4d
zdxzdx1219 <769371987@qq.com>
2020-12-24 09:34:17 +0800

    重新封装 recordListModel.ts

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,20 +1,24 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+    //保存 data
+    data: [] as RecordItem[],
 
-    clone(data: RecordItem[] | RecordItem){
-      return  JSON.parse(JSON.stringify(data))
+    clone(data: RecordItem[] | RecordItem) {
+        return JSON.parse(JSON.stringify(data));
     },
     //读数据 写数据
     fetch() {
         //返回值
-        return  JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordItem[];
+        this.data = JSON.parse(window.localStorage.getItem('localStorageKeyName') || '[]') as RecordItem[];
+        return this.data;
 
-       },
-    save(data: RecordItem[]) {
-        window.localStorage.setItem('localStorageKeyName', JSON.stringify(data));
+    },
+    save() {
+        window.localStorage.setItem('localStorageKeyName',
+            JSON.stringify(this.data));
     }
 
 
 };
-export default recordListModel
+export default recordListModel;
 // export {model};
