92e6fe92bbbfa891aa5561c165174a11c0375229
zdxzdx1219 <769371987@qq.com>
2020-12-24 10:53:39 +0800

    封装 tagList的增删改查

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,11 @@ Vue.component('icon', icon);
 
 window.tagList = tagListModel.fetch();
 
+window.findTag = (id: string) => {
+
+    return window.tagList.filter(t => t.id === id)[0];
+
+};
 window.createTag = (name: string) => {
 
     const message = tagListModel.create(name);
@@ -24,6 +29,14 @@ window.createTag = (name: string) => {
     } else if (message === 'success') {
         window.alert('添加成功');
     }
+};
+window.removeTag = (id: string) => {
+
+    return tagListModel.remove(id);
+};
+window.updateTag = (id: string, name: string) => {
+    return tagListModel.update(id, name);
+
 };
 
 
